0f21c8079fb67f6c696796787ab82c31805bf963
Edon Idrizi <edon.idrizi@hellocare.ai>
2025-09-18 15:41:30 +0200

    Add moshi adapter for new added models

--- app/src/main/java/com/example/myfitcompanion/api/model/ExerciseResponse.kt
+++ app/src/main/java/com/example/myfitcompanion/api/model/ExerciseResponse.kt
@@ -1,9 +1,18 @@
 package com.example.myfitcompanion.api.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ExerciseResponse(
+    @Json(name = "id")
     val id: Int,
+    @Json(name = "name")
     val name: String,
+    @Json(name = "type")
     val type: String,
+    @Json(name = "duration")
     val duration: Int, // in minutes
+    @Json(name = "caloriesBurned")
     val caloriesBurned: Int
 )
--- app/src/main/java/com/example/myfitcompanion/api/model/MealsResponse.kt
+++ app/src/main/java/com/example/myfitcompanion/api/model/MealsResponse.kt
@@ -1,8 +1,16 @@
 package com.example.myfitcompanion.api.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class MealsResponse(
+    @Json(name = "id")
     val id: Int,
+    @Json(name = "name")
     val name: String,
+    @Json(name = "calories")
     val calories: Int,
+    @Json(name = "description")
     val description: String?
 )
