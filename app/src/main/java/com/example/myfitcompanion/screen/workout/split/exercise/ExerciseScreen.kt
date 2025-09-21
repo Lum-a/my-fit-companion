5e508f58bafe910b2e7bb06d19b6f7afb2a35ebf
Edon Idrizi <edonidrizi00@gmail.com>
2025-09-21 16:28:03 +0200

    Add getSplits and getExercises inside userRepository and workoutviewmodel

--- app/src/main/java/com/example/myfitcompanion/screen/workout/split/exercise/ExerciseScreen.kt
+++ app/src/main/java/com/example/myfitcompanion/screen/workout/split/exercise/ExerciseScreen.kt
@@ -1,7 +1,10 @@
 package com.example.myfitcompanion.screen.workout.split.exercise
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.myfitcompanion.screen.workout.WorkoutViewModel
 
 /**
  * Created by Edon Idrizi on 19/Sep/2025 :)
@@ -11,7 +14,12 @@ import androidx.compose.ui.Modifier
 fun ExerciseScreen(
     splitId: Int,
     onBack: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: WorkoutViewModel = hiltViewModel()
 ) {
+
+    LaunchedEffect(Unit) {
+        viewModel.loadExercises(splitId)
+    }
 
 }
